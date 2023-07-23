3bd7820bdf8a2ad3e9a9946d7778ffa2e1935a34
minaton-ru <minaton@yandex.ru>
2023-07-23 21:02:08 +0300

    swagger and fixtures

--- digest/urls.py
+++ digest/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from . import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Digest API",
+      default_version='v1',
+      description="API для получения дайджеста новостей из источников с фильтром по популярности, работает на Django Rest Framework",
+      terms_of_service="https://github.com/minaton-ru/digest-drf",
+      contact=openapi.Contact(email="minaton@yandex.ru"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('digest/', views.CreateDigestView.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- digest/views.py
+++ digest/views.py
@@ -2,9 +2,10 @@ from .models import Post, Digest
 from .serializers import DigestSerializer
 from django.http import Http404
 from django.contrib.auth.models import User
-from django.db.models import Prefetch
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class CreateDigestView(APIView):
     """
@@ -13,12 +14,21 @@ class CreateDigestView(APIView):
     userid - уникальный айди пользователя
     rating - значение рейтинга для фильтра записей
     """
+    serializer_class = DigestSerializer
+
     def get_user(self, id): # Функция для проверки и получения объекта пользователя по его id
         try:
             return User.objects.get(pk=id)
         except User.DoesNotExist:
             raise Http404
-
+    
+    @swagger_auto_schema(manual_parameters=[
+            openapi.Parameter('userid', openapi.IN_QUERY,
+                      description='Уникальный айди пользователя',
+                      type=openapi.TYPE_INTEGER, required=True),
+            openapi.Parameter('rating', openapi.IN_QUERY,
+                      description='Значение рейтинга',
+                      type=openapi.TYPE_INTEGER)])
     def get(self, request, format=None):
         userid = self.request.query_params.get('userid') # Получаем значение id из параметров URL-запроса
         if self.request.query_params.get('rating'): # Получаем значение rating из параметров URL-запроса, если его не было в запросе, то считаем, что рейтинг равен нулю
@@ -27,8 +37,8 @@ class CreateDigestView(APIView):
             rating = 0
         user = self.get_user(userid)
         
-        # Кверисет постов с фильтрами - из подписок этого пользователя и рейтинг постов вышем, чем указанный, исключаем посты, если пользователь их уже видел 
-        posts = Post.objects.exclude(viewed__id=userid).filter(subscription__user=userid).filter(rating__gt=rating) 
+        # Кверисет постов с фильтрами - подписки этого пользователя и рейтинг постов выше или равно, чем указанный, исключая посты, если пользователь их уже видел 
+        posts = Post.objects.exclude(viewed__id=userid).filter(subscription__user=userid).filter(rating__gte=rating) 
         
         new_digest = Digest(user=user)
         new_digest.save() # Сохраняем созданный дайджест
