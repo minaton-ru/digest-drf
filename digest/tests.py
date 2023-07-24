07699967b779b1e3b3cdcc08e41fed219877edc3
minaton-ru <minaton@yandex.ru>
2023-07-24 22:18:04 +0300

    test added

--- digest/tests.py
+++ digest/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 
 
 class StatusCodeTest(TestCase):
@@ -10,18 +10,18 @@ class StatusCodeTest(TestCase):
     def test_404_status(self):
         """
         Должен вернуть код 404, потому что в загруженных фикстурах нет пользователя с id=5
-        """ 
+        """
         response = self.client.get("/api/digest/", {"userid": 5, "rating": 8})
         self.assertEqual(response.status_code, 404)
 
     def test_200_status(self):
         """
         Должен вернуть код 200, потому что в загруженных фикстурах есть пользовател с id=2
-        """ 
+        """
         response = self.client.get("/api/digest/", {"userid": 2, "rating": 8})
         self.assertEqual(response.status_code, 200)
 
-class DigestTest(TestCase):
+class DigestTest(TransactionTestCase):
     """
     Тест количества полученных постов в дайджесте для загруженных фикстур
     """
@@ -29,7 +29,10 @@ class DigestTest(TestCase):
 
     def test_first_digest(self):
         """
-        Должен вернуть дайджест, в котором четыре поста
+        Первый запрос должен вернуть дайджест, в котором четыре поста.
+        Второй запрос для того же пользователя должен вернуть дайджест, в котором три поста.
         """
         response = self.client.get("/api/digest/", {"userid": 2, "rating": 8})
         self.assertEqual(len(response.json()["posts"]), 4)
+        response = self.client.get("/api/digest/", {"userid": 2, "rating": 4})
+        self.assertEqual(len(response.json()["posts"]), 3)
